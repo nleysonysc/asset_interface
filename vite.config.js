684cda0f6e35dc1c1ac0e0fc1d906ed8a282ffa9
nleyson <nleyson@yescharteracademy.org>
2022-11-18 11:55:03 -0800

    modifying components

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { viteSingleFile } from "vite-plugin-singlefile"
-
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), viteSingleFile()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `[name].js`,
+        assetFileNames: `[name].[ext]`,
+        manualChunks: undefined
+      }
+    }
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
