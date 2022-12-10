7ead43c648c6084e18e94d1b467c82e6cc794fcf
Nick <nleyson@yescharteracademy.org>
2022-12-10 10:11:24 -0800

    adding appscript test button

--- src/components/Screens/Home.vue
+++ src/components/Screens/Home.vue
@@ -1,7 +1,15 @@
 <script setup>
+  import { ref } from 'vue'
 
+  let textVal = ref('')
+
+  function handleClick(e){
+    console.log(e)
+  }
 </script>
 
 <template>
   Home page
+  <button @click="google.script.run.withSuccessHandler(handleClick).textSearch('edom')">test</button>
+  <span>{{textVal}}</span>
 </template>
